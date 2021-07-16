ffe60fdf0f21f79ba6d41cf2fbb9f4af36dd4971
Yuma Shintani <yuu7taku@icloud.com>
2021-07-17 04:24:43 +0900

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'TsumTsumYuma',
+  description: 'Just playing around',
+  locales: {
+    '/': {
+      lang: 'ja'
+    }
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about.html' },
+      { text: 'GitHub', link: 'https://github.com/yuma-shin' }
+    ],
+    sidebar: {
+      // fallback
+      '/': [
+        '',        /* / */
+      ]
+    }
+  },
+  base: '/viewhp/',
+  dest: 'docs'
+}
+
